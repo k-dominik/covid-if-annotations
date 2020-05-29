3fa928a6ae4af61fd9c3029facbb75c886d76214
kdominik <k-dominik@users.noreply.github.com>
2020-05-29 22:40:36 +0200

    windows 0.0.2dev7 release

--- conda-recipe/bld.bat
+++ conda-recipe/bld.bat
@@ -4,7 +4,7 @@ if errorlevel 1 exit 1
 set MENU_DIR=%PREFIX%\Menu
 IF NOT EXIST (%MENU_DIR%) mkdir %MENU_DIR%
 
-copy %RECIPE_DIR%\covid-if2.ico %MENU_DIR%\
+copy %RECIPE_DIR%\icon_256x256.ico %MENU_DIR%\
 if errorlevel 1 exit 1
 
 copy %RECIPE_DIR%\menu.json %MENU_DIR%\covid-shortcut.json
